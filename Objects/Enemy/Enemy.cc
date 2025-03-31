7b464b6ecf98543c30c6df15f213aaba2e2c7a1c
Jamie Chareunsouk <jachareu@uwaterloo.ca>
2025-03-31 04:32:29 -0400

    Merging Board w Other Implementations

--- Objects/Enemy/Enemy.cc
+++ Objects/Enemy/Enemy.cc
@@ -14,8 +14,6 @@ export class Enemy: public LivingEntity {
         void attack() override;
 
         virtual void onDeath(); // params: Board& b
-
-        void onDeath(); // params: Board& b
         void giveCompass();
         // observer methods
         void notify(Subject& whoNotified, std::string action) override; // is called when someone interacts with me.
